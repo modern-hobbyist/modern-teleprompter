aebdff815c48dbeca3cf80115bb2b94d4c1f6aac
Charlie Steenhagen <csteen1005@gmail.com>
2020-03-16 12:44:14 -0400

    Update transcript.blade.php

--- resources/views/frontend/transcript.blade.php
+++ resources/views/frontend/transcript.blade.php
@@ -142,7 +142,7 @@
 @section('content')
     <div class="row">
         <div class="transcript">
-            <p>{{ $contents }}</p>
+            <p>{!! $contents !!}</p>
         </div>
     </div>
     <div class="controls">
